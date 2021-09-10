cf364a44d7a88902b4352da6e3fd13bf80bbed13
Jopat2409 <75854642+Jopat2409@users.noreply.github.com>
2021-09-10 11:01:37 +0100

    Added method to draw the current beatmap selection

--- BeatmapSelect.py
+++ BeatmapSelect.py
@@ -1,4 +1,4 @@
-
+import random
 import pickle
 import os
 import config
@@ -21,10 +21,15 @@ class gsBeatmapSelect:
 
         self.beatmaps = []
 
+
+
         cPath = bgPath = config.DEFAULT_PATH + '/temp/beatmapSelect.obj'
         bgPath = config.DEFAULT_PATH + '/assets/bg/online_background_68261587a4e3fbe77cad07120ee1e864.jpg'
         bg = pygame.image.load(bgPath)
         self.bgIMG = pygame.transform.scale(bg, config.SCREEN_RESOLUTION)
+
+        # hard coded number representing how many beatmaps are shown on screen at once
+        self.BMS = 7
 
         if os.path.isfile(cPath):
             with open(cPath, 'r') as file:
@@ -40,13 +45,46 @@ class gsBeatmapSelect:
                 self.beatmaps.append(beatmapTemp)
 
         print(self.beatmaps)
+        # points to the current index of the beatmap currently selected
+        self.cBeatmap = random.randint(0, len(self.beatmaps))
 
+
+    def update(self):
+
+
+        pass
+
+
+    def drawBeatmapRects(self, surface):
+
+        drawnbeatmaps = []
+        drawCount = 0
+        for beatmap in range(self.cBeatmap - int((self.BMS-1)/2), self.cBeatmap+int((self.BMS-1)/2)+1):
+            try:
+                cData = self.beatmaps[beatmap]
+            except IndexError:
+                break
+        pygame.draw.rect(surface, )
+
+        drawCount += 1
+
+            
+
+        
+
+        
 
     def getRenderSnapshot(self, interpolation):
 
         tempSurface = pygame.Surface(config.SCREEN_RESOLUTION)
         tempSurface.blit(self.bgIMG, (0,0))
 
+        beatmapFrame = pygame.Surface((config.SCREEN_RESOLUTION[0] / 3, config.SCREEN_RESOLUTION[1]))
+
+        beatmapFrame.fill((0,0,0))
+        self.drawBeatmapRects(beatmapFrame)
+
+        tempSurface.blit(beatmapFrame, ((config.SCREEN_RESOLUTION[0] / 3)*2,0))
 
         return tempSurface
                 
