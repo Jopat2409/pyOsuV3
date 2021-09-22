3012aef7090dc1e06e70ed5f7f0cdfed81737d4f
Joe <joantpat@gmail.com>
2021-09-22 14:10:31 +0100

    Updated timing points

--- BeatmapPlay.py
+++ BeatmapPlay.py
@@ -11,76 +11,99 @@ class gsBeatmapPlayer:
 
     def __init__(self, PATH, parentClass):
 
+        # pointer to the parent class
         self.parent = parentClass
+        # pointer to the sound handler used for playing music etc
         self.soundHandler = parentClass.soundStream
-        print(PATH)
+        #print(PATH)
+        # load the data from the beatmap file which will be used to display and run the map
         self.playingBeatmap = PATH
         self.hitObjects = BeatmapParse.fullParse(self.playingBeatmap["osuPath"])
-        print(self.hitObjects)
+        #print(self.hitObjects)
 
+        # calculate all necessary values for the map, such as relative circle size, approach rate in ms etc
         self.getMapValues()
 
+        # load and display the background image assigned to the map
         PATH = os.path.join(self.playingBeatmap["BasePath"],self.playingBeatmap["BackgroundImage"])
         PATH = PATH.replace("\\","/")
         bg = pygame.image.load(PATH.strip())
+        # transform the image to the size of the screen currently
         self.bgIMG = pygame.transform.scale(bg, config.SCREEN_RESOLUTION)
 
+        # initialize the font used throught the map
         self.cFont = pygame.font.SysFont('Arial', 40)
 
-        self.KEY_MAP = {"keyOsuLeft":self.checkTimingPoints,
-                               "keyOsuRight":self.checkTimingPoints}
+        # mapping all key presses to their respective functions
+        self.KEY_MAP = {"keyOsuLeft":self.hasHitNextNote,
+                               "keyOsuRight":self.hasHitNextNote}
 
+        # holds the value of the current combo held by the player
         self.combo = 0
+        # holds the value of the maximum combo achieved by the player
         self.maxCombo = 0
+        # holds the value of the number of misses held by the player
         self.missCount = 0
+        # holds the value of the current score
         self.score = 0
-
+        
         self.hitTimings = []
 
+        # plays the song and begins the beatmap
         self.soundHandler.playSong(self.playingBeatmap["BasePath"]+"/"+self.playingBeatmap["AudioFilename"])
 
-
+    """ Function called when a successful circle press is registererd"""
     def hitNote(self, score):
-
+        # increment the player's current combo
         self.combo += 1
+        # if the current combo is larger than the max combo, update the max combo
         if self.combo > self.maxCombo:
             self.maxCombo = self.combo
-
+        # add to the user's current score the current combo multiplied by the hit score achieved (300, 100, 50)
         self.score += self.combo*score
+        self.hitObjects.pop(0)
 
+    """ Function called when a missed note is registered"""
     def missNote(self):
         
         self.missCount += 1
         self.combo = 0
+        self.hitObjects.pop(0)
         
 
         
 
         
 
-
+    """ Function that calculates all the necessary values used in the playing of the beatmap"""
     def getMapValues(self):
 
         # calculate the AR timings
+        """ If the approach rate is less than 5, the formula to work it out is 800+400*(5-approach rate)/5"""
         if float(self.playingBeatmap["ApproachRate"]) < 5:
             self.fadeInTime = int(800 + 400*((5-float(self.playingBeatmap["ApproachRate"])) / 5))
             self.fadeInStart = int(1200 + 600*((5-float(self.playingBeatmap["ApproachRate"])) / 5))
         elif float(self.playingBeatmap["ApproachRate"]) == 5:
+            """ If the approach rate is 5, the formula is just 800"""
             self.fadeInTime = 800
             self.fadeInStart = 1200
         elif float(self.playingBeatmap["ApproachRate"]) > 5:
+            """ If the approach rate is greater than 5, the formula is 800-500*(appraoch rate - 5) / 5"""
             self.fadeInTime = int(800 - 500*((float(self.playingBeatmap["ApproachRate"])-5)/5))
             self.fadeInStart = int(1200 - 750*((float(self.playingBeatmap["ApproachRate"])-5)/5))
-
+        # calculate the outer radius of the circle based on the current circle size
         self.radius = int(config.CURRENT_SCALING*(54.4-4.48*float(self.playingBeatmap["CircleSize"])))
         self.innerRadius = int(self.radius*0.9)
         self.hCircleRadius = self.radius*2
 
+        # calculate the respective windows for scores, using the map's overall difficulty value
         self.hitWindows = [((400-20*float(self.playingBeatmap["OverallDifficulty"]))/2)+250,
                            ((280-16*float(self.playingBeatmap["OverallDifficulty"]))/2)+250,
                            ((160-12*float(self.playingBeatmap["OverallDifficulty"]))/2)+250]
-
+        
         self.scoreWindows = [50,100,300]
+
+        # calculate values related to the hit timing display
         self.rectSize = int(config.SCREEN_RESOLUTION[0] / 3)
         self.timingColors = [(255,0,0),(0,255,0),(0,0,255)]
         #print(self.hitWindows)
@@ -88,6 +111,9 @@ class gsBeatmapPlayer:
         self.xOffset = int((config.SCREEN_RESOLUTION[0] - config.DEFAULT_RESOLUTION[0]*config.CURRENT_SCALING)/2)
 
         #print("X offset: {}".format(self.xOffset))
+
+        # ms before the hit window begins in which a player misses if they try to hit the circle
+        self.missMargin = 50
 
 
     def drawFollowPoints(self):
@@ -173,45 +199,59 @@ class gsBeatmapPlayer:
         else:
             return False
 
+    def hasHitNextNote(self):
+
+        """ For the note to have been hit, the user must have had both the cursor over the circle, and must have pressed the z or x button
+        within the required timing points."""
+
+        # if the user's mouse was not over the circle, no action should be taken
+        if not self.checkCircle():
+            return
+
+        score = self.checkTimingPoints()
+        # if the user's hit was not within any timing points, ignore it
+        if score == None:
+            return
+        elif score == 0:
+            self.missNote()
+        else:
+            self.hitNote(score)
+        
+
         
 
     def checkTimingPoints(self):
 
 
-
+        # get the current position in ms 
         cPos = self.soundHandler.musicChannel.get_pos()
 
         #print("Checking Timing Points")
 
+        # calculate the difference between the current position and the timing point
         cDiff = cPos - self.hitObjects[0][2]
 
+        # if the mouse overlaps with the circle but the timing is within the miss timing point, return 0
+        if abs(cDiff) >= self.hitWindows[0] and abs(cDiff) <= self.hitWindows[0]+self.missMargin:
+            return 0
+        # if the differenece is greater than the miss window, ignore
+        elif abs(cDiff) > self.hitWindows[0] + self.missMargin:
+            return None
+
+        # work out which score value to assign to the player
         scoresHit = []
         for i in range(len(self.hitWindows)):
             if abs(cDiff) < self.hitWindows[i]:
                 scoresHit.append(self.scoreWindows[i])
             
-
-        if len(scoresHit) == 0:
-            pass
-        else:
-            if self.checkCircle():
-                if len(self.hitTimings) >= 25:
-                    self.hitTimings.pop()
-                self.hitTimings.append(cDiff)
-                self.hitNote(max(scoresHit))
-                #print("hit note {}".format(max(scoresHit)))
-            else:
-                self.missNote()
-
-            self.hitObjects.pop(0)
+        return max(scoresHit)
                 
             
 
 
     def update(self):
 
-        if int(self.hitObjects[0][2])+self.hitWindows[0] < self.soundHandler.musicChannel.get_pos():
-            self.hitObjects.pop(0)
+        if int(self.hitObjects[0][2]) < self.soundHandler.musicChannel.get_pos():
             self.missNote()
 
         
