35d5bd0bd1f812f7e8670509202c071642b62809
Joe <joantpat@gmail.com>
2021-12-07 17:37:40 +0000

    Updates to UI

--- UiManager/UiMain.py
+++ UiManager/UiMain.py
@@ -1,5 +1,6 @@
 import pygame
 from UiObjects import FLAGS as flags
+import os
 
 class UidGenerator:
 
@@ -21,76 +22,127 @@ class UidGenerator:
         self._usableIds.append(_id)
         
 
-
-
-
-
 class UiHandler:
 
+    def __init__(self):
 
+        self.ID_GEN = UidGenerator()
+
+        # self._components[flags.COMPONENT] = {id:component}
+        self._components = [{} for flag in flags]
+
+        self.defaultTexture = pygame.image.load(os.path.join("c:/Users/Joe/Documents/GitHub/pyOsuV3/UiManager/","assets", "ape.jpg")).convert()
+
+        self.eventMap ={
+            pygame.MOUSEBUTTONUP:self.onMouseRelease,
+            pygame.MOUSEBUTTONDOWN:self.onMousePress
+
+        }
+
+        self.lastMousePos = pygame.mouse.get_pos()
+        self.activeMouseComponent = None
+
+    """
+    Adds a basic element to the UI
+    imageUrl: url of image to display
+    postion: tuple containing (x,y)
+    customRect: used for scaling
+    SHOW: used for default showing status
+    """
+    def addElement(self, imageUrl, position, customRect=None, SHOW=1):
+
+        # generate an id for the element
+        newId = self.ID_GEN.getNextId()
+        
+        #try and load the image provided
+        try:
+            image = pygame.image.load(imageUrl).convert()
+        except FileNotFoundError:
+            # if the image cant be found, use the default texture if no custom size is set
+            if not customRect:
+                image = self.defaultTexture
+            # else resize the default texture to the correct size
+            else:
+                image = pygame.image.transform(self.defaultTexture, (customRect.w, customRect.h))
+
+        # add the render component for rednering the ui object
+        self._components[flags.UX_RENDER].update({newId:[image,position,SHOW]})
+        # add the position element for detecting collisions
+        self._components[flags.UX_POS].update({newId:image.get_rect()})
+        
+    
+        # return the new Id to the user so they can manipulate the element later
+        return newId
     
 
+    def handleEvent(self, pygameEvent):
 
-    def __init__(self):
+        eventType = pygameEvent.type
+        if eventType in self.eventMap:
+            self.eventMap[eventType]()
         
-        self._components = [{} for i in flags]
-        self.idGen = UidGenerator()
 
 
+    def addEvent(self, _id, eventFlag, eventData):
 
-    def addElement(self, renderComponent):
-
-        newId = self.idGen.getNextId()
+        if eventFlag not in flags:
+            return
         
-        self._components[flags.UX_RENDER].update({newId:renderComponent})
-        self._components[flags.UX_POS].update({newId:renderComponent[0].get_rect()})
-
-        self._componentHandlers = {pygame.MOUSEBUTTONDOWN:self.handleMousePress,
-                                   pygame.MOUSEBUTTONUP:self.handleMouseRelease}
-
-        return newId
-
-    def removeElement(self, _id):
-
-        for _componentDict in self.components:
-            del _componentDict[_id]
-
-        self.idGen.returnId(_id)
-
-    def handleEvent(self, event):
-        if event.type in self._componentHandlers:
-            self._componentHandlers[event.type]()
-        
+        self._components[eventFlag].update({_id:eventData})
+    
 
     def render(self, surface):
 
-        for renderObject in self._components[flags.UX_RENDER].values():
-            if renderObject[2]:
-                surface.blit(renderObject[0], renderObject[1])
-
-    def addEvent(self, _id, eventType, function):
-        
-        if eventType in flags:
-            eventRect = self._components[flags.UX_POS][_id]
-            self._components[eventType].update({_id:[eventRect,function]})
-    def handleMouseEvent(self, uxElement):
-        mPos = pygame.mouse.get_pos()
-        for _object in self._components[uxElement].values():
-            if _object[0].collidepoint(mPos):
-                _object[1]
-            
-
-    def handleMouseRelease(self):
-        self.handleMouseEvent(flags.UX_ONCLICK)
-
-    def handleMousePress(self):
-        mPos = pygame.mouse.get_pos()
-        pass
+        for _object in self._components[flags.UX_RENDER].values():
+            if _object[2]:
+                surface.blit(_object[0], _object[1])
 
     def update(self):
-        self.handleMouseMove(self)
-
-    def handleMouseMove(self):
-
-        self.handleMouseEvent(flags.UX_ONHOVER)
         
+        if self.checkMouseMove():
+            self.getActiveMouseComponent()
+            self.onMouseMove()
+        
+        
+    """ -------------- FUNCTIONS TO DO WITH MOUSE INPUT HANDLING -----------------"""
+    def onMousePress(self):
+        pass
+
+    def onMouseRelease(self):
+        if self.activeMouseComponent and self.activeMouseComponent in self._components[flags.UX_ONCLICK]:
+            self._components[flags.UX_ONCLICK][self.activeMouseComponent]()
+
+    def onMouseMove(self):
+        pass
+
+    def checkMouseMove(self):
+        mousePos = pygame.mouse.get_pos()
+        if mousePos == self.lastMousePos:
+            return False
+        else:
+            self.lastMousePos = mousePos
+            return True
+    
+    """
+    Gets the current component that is applicable to mouse functions
+    """
+    def getActiveMouseComponent(self):
+        # get the current mouse position
+        currentMousePos = pygame.mouse.get_pos()
+        # referenece to the position components
+        tempPositionComponents = self._components[flags.UX_POS]
+
+        # iterate through the key,value pair objects 
+        for objectId, _object in tempPositionComponents.items():
+            # if the mouse position is within the current mouse position
+            if _object.collidepoint(currentMousePos):
+                # set the active mouse component
+                self.activeMouseComponent = objectId
+                return
+
+        self.activeMouseComponent = None
+    
+
+
+    """ --------------------------------------------------------------------------"""
+
--- UiManager/testUI.py
+++ UiManager/testUI.py
@@ -3,11 +3,12 @@ import UiMain
 import UiObjects
 import os
 from UiObjects import FLAGS as flags
+import time
 
 pygame.init()
 
 def doSomething():
-    print("worked")
+    print("Clicked")
 
 def hover():
     print("hovering")
@@ -17,11 +18,21 @@ sf = pygame.display.set_mode((1000,700))
 sf.fill(0)
 ui = UiMain.UiHandler()
 
-apeButton = ui.addElement([pygame.image.load(os.path.join("assets", "ape.jpg")), (10,10), 1])
+apeButton = ui.addElement(os.path.join("c:/Users/Joe/Documents/GitHub/pyOsuV3/UiManager/","assets", "ape.jpg"), (10,10), 1)
 ui.addEvent(apeButton, flags.UX_ONCLICK, doSomething)
 ui.addEvent(apeButton, flags.UX_ONHOVER, hover)
 
+updates = 0
+prevTime = time.time()
 while(True):
+
+    eTime = time.time() - prevTime
+
+    if time.time() - prevTime >= 1:
+        prevTime = time.time()
+        print(updates)
+        updates = 0
+
 
     for event in pygame.event.get():
         
@@ -31,5 +42,6 @@ while(True):
             break
         else:
             ui.handleEvent(event)
-        ui.update()
-        ui.render(sf)
+    ui.update()
+    ui.render(sf)
+    updates+=1
